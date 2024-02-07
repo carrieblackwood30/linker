460171f1829b3af3ceaf7fc42587a5e0ba9f820d
unknown <carrieblackwood30@gmail.com>
2024-02-07 12:16:52 +0600

    let beta test

--- script.js
+++ script.js
@@ -33,19 +33,54 @@ createPostBtn.addEventListener('click', () =>{
 
     posts.append(createPost)
 
+    let createCloseBtn = document.createElement('img')
+    createCloseBtn.classList.add('close-btn')
+    createCloseBtn.src = `https://cdn1.iconfinder.com/data/icons/simple-ui-11/32/100_icon_set-14-1024.png`
+   
+    createCloseBtn.addEventListener('click', function(event){
+        event.stopPropagation()
+        createPost.remove()
+    })
+
     let heading = prompt('your heading')
-    
     let createHeading = document.createElement('h3')
     createHeading.classList.add('head')
     createHeading.textContent = heading
 
-    createPost.append(createHeading)
-
     let paragraph = prompt('your paragraph')
-
     let createParagraph = document.createElement('p')
     createParagraph.classList.add('paragraph')
     createParagraph.textContent = paragraph
 
-    createPost.append(paragraph)
+    let createColorPicker = document.createElement('input')
+    createColorPicker.type = 'color'
+    createColorPicker.classList.add('color-picker')
+    createColorPicker.addEventListener('input', function(){
+        createPost.style.color = createColorPicker.value
+    })
+
+    let createLinkArea = document.createElement('a')
+    createLinkArea.style.display = 'block'
+
+    let createRandomLinkerBtn = document.createElement('button')
+    createRandomLinkerBtn.classList.add('createLink')
+    createRandomLinkerBtn.innerText = 'create a link'
+    createRandomLinkerBtn.addEventListener('click', () =>{
+    
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        const charactersSplitter = characters.split('')
+        let randomCharacters = charactersSplitter.sort(() => Math.random() - 0.5)
+        let finalCharacters = randomCharacters.join('')
+        
+        
+        createLinkArea.href = characters 
+        createLinkArea.innerText = `linker.com/post/${finalCharacters}.com`
+    })
+
+    createPost.append(createHeading)
+    createPost.append(createParagraph)
+    createPost.append(createColorPicker)
+    createPost.append(createRandomLinkerBtn)
+    createPost.append(createLinkArea)
+    createPost.append(createCloseBtn)
 })
